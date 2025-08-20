069601faeab81e1cb90fcdcf4066c0d1bbfd79e8
Steponass <steponas.dabuzinskas@gmail.com>
2025-08-20 15:07:21 +0300

    Visual polish, buttons first.

--- src/components/ui/LevelTitle/LevelTitle.jsx
+++ src/components/ui/LevelTitle/LevelTitle.jsx
@@ -1,19 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import styles from './LevelTitle.module.css';
+import React, { useState, useEffect } from "react";
+import styles from "./LevelTitle.module.css";
 
 const LevelTitle = ({ levelId, levelTitle }) => {
   const [showDisplay, setShowDisplay] = useState(false);
-  const [currentDisplayLevel, setCurrentDisplayLevel] = useState(null);
 
   useEffect(() => {
     if (levelId) {
-      setCurrentDisplayLevel(levelId);
       setShowDisplay(true);
-      
 
       const timer = setTimeout(() => {
         setShowDisplay(false);
-      }, 4000);
+      }, 5000);
 
       return () => clearTimeout(timer);
     }
@@ -25,8 +22,8 @@ const LevelTitle = ({ levelId, levelTitle }) => {
 
   return (
     <div className={styles.levelDisplay}>
-        <h1 className={styles.levelNumber}>Level {levelId}</h1>
-        <h2 className={styles.levelTitle}>{levelTitle}</h2>
+      <h1 className={styles.levelNumber}>Level {levelId}</h1>
+      <h2 className={styles.levelTitle}>{levelTitle}</h2>
     </div>
   );
 };
