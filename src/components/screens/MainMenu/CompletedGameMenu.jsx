224c73759e26fbbae1bee5d8d1c5607a1403d331
Steponass <steponas.dabuzinskas@gmail.com>
2025-10-08 08:02:29 +0300

    1. Adjusted CompletedGameMenu button size + layout to fit in small screens.

--- src/components/screens/MainMenu/CompletedGameMenu.jsx
+++ src/components/screens/MainMenu/CompletedGameMenu.jsx
@@ -1,38 +1,37 @@
-import React from 'react';
-import { LEVEL_METADATA } from './levelMetadata';
-import ActionButton from '@components/ui/ActionButton/ActionButton';
-import ThresholdAdjuster from '@components/ui/ThresholdAdjuster/ThresholdAdjuster';
-import styles from './MainMenu.module.css';
+import React from "react";
+import { LEVEL_METADATA } from "./levelMetadata";
+import ActionButton from "@components/ui/ActionButton/ActionButton";
+import ThresholdAdjuster from "@components/ui/ThresholdAdjuster/ThresholdAdjuster";
+import styles from "./MainMenu.module.css";
 
 const CompletedGameMenu = ({ onLevelSelect, onStartFresh }) => {
   return (
     <div className={styles.menuContent}>
-      <h1 className={styles.title}>Mystify Me</h1>
+      <h1 className={styles.completed_title}>Mystify Me</h1>
 
       <ThresholdAdjuster />
 
       <div className={styles.levelSelection}>
-        {LEVEL_METADATA.map((level) => (
-          <ActionButton
-            key={level.title}
-            className={styles.levelButton}
-            onClick={() => onLevelSelect(level.path)}
-            variant="level"
-          >
-            <p>{level.title}</p>
-          </ActionButton>
-        ))}
+        {/* filter levels with numerical ids to not render "tutorial" */}
+        {LEVEL_METADATA.filter((level) => typeof level.id === "number").map(
+          (level) => (
+            <ActionButton
+              key={level.title}
+              className={styles.levelButton}
+              onClick={() => onLevelSelect(level.path)}
+              variant="level"
+            >
+              <p>{level.title}</p>
+            </ActionButton>
+          )
+        )}
       </div>
 
       <div className={styles.freshStartContainer}>
-        <ActionButton
-          variant="primary"
-          onClick={onStartFresh}
-        >
+        <ActionButton variant="primary" onClick={onStartFresh}>
           Start Fresh
         </ActionButton>
       </div>
-      
     </div>
   );
 };
